571461a1db835d4c49087b76a9732b13b85044ba
Zijian Zhang <zzhang38@terpmail.umd.edu>
2021-01-06 23:19:38 -0500

    Unit-00

---
+++ src/logic/Model.java
@@ -0,0 +1,13 @@
+package logic;
+
+public class Model {
+	// implement listener
+	
+	// 判断是点的子还是要走还是另外一个子
+	
+	// 特判过路兵
+	
+	// 走完判游戏结束了没
+	
+	// setMovableFlags
+}
---
+++ src/logic/View.java
@@ -0,0 +1,9 @@
+package logic;
+
+public class View {
+	// ½¨piece[][], boardView, Panel[][], Label[][]
+	
+	// Listeners
+	
+	// get()
+}
